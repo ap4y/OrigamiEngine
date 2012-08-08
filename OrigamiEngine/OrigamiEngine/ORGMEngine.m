1a31daae899467f87f36223c78c6542d3706d397
ap4y <lod@pisem.net>
2012-08-08 23:49:15 +0400

    pushed initialisation to the background thread

--- OrigamiEngine/OrigamiEngine/ORGMEngine.m
+++ OrigamiEngine/OrigamiEngine/ORGMEngine.m
@@ -32,23 +32,25 @@
 #pragma mark - public
 
 - (void)playUrl:(NSURL*)url {
-    self.input = [[ORGMInputUnit alloc] init];
-    if (![_input openWithUrl:url]) {
-        @throw [NSException exceptionWithName:NSInternalInconsistencyException
-                                       reason:NSLocalizedString(@"Couldn't open source...", nil)
-                                     userInfo:nil];
-	}
-
-    self.converter = [[ORGMConverter alloc] initWithInputFormat:_input];
-    self.output = [[ORGMOutputUnit alloc] initWithConverter:_converter];
-    
-    if (![_converter setupWithOutputUnit:_output]) {
-        @throw [NSException exceptionWithName:NSInternalInconsistencyException
-                                       reason:NSLocalizedString(@"Couldn't setup converter...", nil)
-                                     userInfo:nil];
-	}
-    
-    dispatch_source_merge_data([ORGMQueues buffering_source], 1);
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT , 0), ^{
+        self.input = [[ORGMInputUnit alloc] init];
+        if (![_input openWithUrl:url]) {
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:NSLocalizedString(@"Couldn't open source...", nil)
+                                         userInfo:nil];
+        }
+        
+        self.converter = [[ORGMConverter alloc] initWithInputFormat:_input];
+        self.output = [[ORGMOutputUnit alloc] initWithConverter:_converter];
+        
+        if (![_converter setupWithOutputUnit:_output]) {
+            @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                           reason:NSLocalizedString(@"Couldn't setup converter...", nil)
+                                         userInfo:nil];
+        }
+        
+        dispatch_source_merge_data([ORGMQueues buffering_source], 1);
+    });
 }
 
 - (void)pause {
