9ec701b33fd7c9ef319093f3f02ae854ed9f5616
ap4y <lod@pisem.net>
2012-08-12 18:23:46 +0400

    fixed seek error issue for the flac decoder

--- OrigamiEngine/OrigamiEngine/Plugins/FlacDecoder.m
+++ OrigamiEngine/OrigamiEngine/Plugins/FlacDecoder.m
@@ -63,7 +63,8 @@
 	int bytesPerFrame = (bitsPerSample/8) * channels;
 	while (framesRead < frames) {
 		if (blockBufferFrames == 0) {
-			if (FLAC__stream_decoder_get_state (decoder) == FLAC__STREAM_DECODER_END_OF_STREAM) {
+			if (FLAC__stream_decoder_get_state(decoder) == FLAC__STREAM_DECODER_END_OF_STREAM ||
+                FLAC__stream_decoder_get_state(decoder) == FLAC__STREAM_DECODER_SEEK_ERROR) {
 				break;
 			}
             
