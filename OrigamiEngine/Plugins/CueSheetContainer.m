811d955d50c517f010018b997e3d9b1d3c55a208
ap4y <lod@pisem.net>
2012-10-30 13:03:43 +0400

    cue sheet container logic changed: auto assign cue extension instead of any other

--- OrigamiEngine/Plugins/CueSheetContainer.m
+++ OrigamiEngine/Plugins/CueSheetContainer.m
@@ -5,19 +5,23 @@
 
 #pragma mark - ORMGContainer
 
-+ (NSArray*)fileTypes {
++ (NSArray *)fileTypes {
 	return [NSArray arrayWithObject:@"cue"];
 }
 
-+ (NSArray*)urlsForContainerURL:(NSURL*)url {
++ (NSArray *)urlsForContainerURL:(NSURL *)url {
 	NSMutableArray *tracks = [NSMutableArray array];	
 	CueSheet *cuesheet = [[CueSheet alloc] initWithURL:url];
     [cuesheet.tracks enumerateObjectsUsingBlock:^(CueSheetTrack* track,
                                                   NSUInteger idx,
-                                                  BOOL *stop) {
-        NSString* appendString = [NSString stringWithFormat:@"%@%@",
-                                  singleFileCueSeparator, [track track]];
-        NSURL* formattedUrl = [track.url URLByAppendingPathComponent:appendString];
+                                                  BOOL *stop) {        
+        NSURL* saveURL = track.url;
+        NSURL* parentDirectory = [saveURL URLByDeletingLastPathComponent];
+        NSString* fileName = [saveURL.lastPathComponent stringByDeletingPathExtension];
+        
+        NSString *newFileName = [NSString stringWithFormat:@"%@.cue%@%@",
+                                 fileName, singleFileCueSeparator, [track track]];
+        NSURL* formattedUrl = [NSURL URLWithString:newFileName relativeToURL:parentDirectory];        
         [tracks addObject:formattedUrl];
     }];
 
