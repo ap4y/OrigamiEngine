b154f750f75c6d81d0e2317bd700b90f46a1b29a
ap4y <lod@pisem.net>
2013-02-10 09:52:32 +1300

    osx example added

---
+++ Example/OSX/OrigamiEngine Example/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ORGMAppDelegate.h
+//  OrigamiEngine Example
+//
+//  Created by Arthur Evstifeev on 9/02/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSView *mainView;
+
+@end
---
+++ Example/OSX/OrigamiEngine Example/PlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ORGMPlayerViewController.h
+//  OrigamiEngine Example
+//
+//  Created by Arthur Evstifeev on 9/02/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PlayerViewController : NSViewController
+
+@end
